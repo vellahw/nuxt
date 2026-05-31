d7ddfaf4d8139e7b5a5855a3de7d4ec3a5e64fed
Vella <112395421+vellahw@users.noreply.github.com>
2026-05-31 22:34:29 +0900

    기본 구조

---
+++ app/pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    <p>이 페이지는 /about 라우트에서 표시됩니다.</p>
+  </section>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> 이것은 자동 import된 컴포넌트입니다 </AppAlert>
+  </div>
+</template>
