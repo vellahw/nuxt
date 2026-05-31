ae956c5bd8dae911dd1d251cd8021b7ae9e6a644
Vella <112395421+vellahw@users.noreply.github.com>
2026-05-31 22:46:39 +0900

    스타일링 방법

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Welcome to the homepage</h1>
+    <h1 class="title">Welcome to the homepage</h1>
     <!-- public 폴더 -->
     <img src="/img/me.png" alt="image" />
     <!-- assets 폴더 -->
@@ -8,3 +8,7 @@
     <AppAlert> 이것은 자동 import된 컴포넌트입니다 </AppAlert>
   </div>
 </template>
+
+<style>
+@import url("~/assets/css/index.css");
+</style>
