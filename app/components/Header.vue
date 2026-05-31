b9b218817574893b891af8285c7a17d84f0d1836
Vella <112395421+vellahw@users.noreply.github.com>
2026-05-31 23:27:09 +0900

    상태관리

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -3,5 +3,6 @@
     <h1>App Header</h1>
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/counter">Counter</NuxtLink>
   </header>
 </template>
---
+++ app/pages/counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>카운터: {{ counter }}</h1>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+  </div>
+  <div>
+    <h1>전역 상태 색상: {{ color }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const counter = useState("counter", () => Math.round(Math.random() * 100));
+const color = useColor(); // 전역 상태
+</script>
