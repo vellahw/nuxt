a98bd987a06c61d84d3df5bb2f5bd799e8071356
Vella <112395421+vellahw@users.noreply.github.com>
2026-05-31 23:04:20 +0900

    화면 전환 트랜지션

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/default.css"],
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
 });
