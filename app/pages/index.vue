7326b5ddba73da2cffcc0a24dab2b6a953921069
vella <gksdnjs2@gmail.com>
2026-06-02 14:12:54 +0900

    중첩 컴포저블 폴더

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import Button from "~/components/base/foo/Button.vue";
+import { useFoo } from "~/composables/base/useFoo";
+
+const foo = useFoo();
 </script>
 
 <template>
@@ -7,6 +10,7 @@ import Button from "~/components/base/foo/Button.vue";
     <h1 class="title">Welcome to the homepage</h1>
     <AppAlert> 이것은 자동 import된 컴포넌트입니다 </AppAlert>
     <Button />
+    중첩 composables 폴더 {{ foo }}
   </div>
 </template>
 
